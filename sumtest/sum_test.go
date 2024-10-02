d09f6daa1135615b353e34f6a8fa95faf8f6e97e
Justin Wu <justinwu9090@gmail.com>
2024-10-02 15:21:30 -0700

    test sum using array

---
+++ sumtest/sum_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+// arrays have a fixed capacity which is defined at declaration.
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+	// numbers := [...]int{1,2,3,4,5}
+	got := Sum(numbers)
+	want := 15
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
