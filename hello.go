aa972f41c1dd79e49a4335af09fa096f4f2421b5
Justin Wu <justinwu9090@gmail.com>
2024-10-02 14:39:39 -0700

    refactor using constant

--- hello.go
+++ hello.go
@@ -1,11 +1,11 @@
 // package to group up my code
 package main
 
-// importing io package
+const englishHelloPrefix = "Hello, "
 
 // function Hello returns a string
 func Hello(name string) string { // parameters are name type instead of type name
-	return "Hello, " + name // no semicolon
+	return englishHelloPrefix + name // no semicolon
 }
 
 // main function called in start
