71b4d113ca8f4f449aaad3e0adc846a764421880
Justin Wu <justinwu9090@gmail.com>
2024-10-02 16:34:04 -0700

    created methods

--- structsmethodsinterfaces/perimeter.go
+++ structsmethodsinterfaces/perimeter.go
@@ -1,10 +1,43 @@
 package structsmethodsinterfaces
 
-func Perimeter(width, height float64) (ans float64) {
-	ans = 2 * (width + height)
+import "math"
+
+// interface declaration which requires method return float
+type Shape interface {
+	Area() float64
+}
+
+// structs declared
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) (ans float64) {
+	ans = 2 * (rectangle.Width + rectangle.Height)
 	return
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
+
+// overloading function calling arguments is not legal in Golang, instead you
+// can declare functions of the same name in different packages.
+// but that's overkill here as well
+
+// methods:
+// converted func Area() to method Area() which can be called on
+// different variable types.
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
