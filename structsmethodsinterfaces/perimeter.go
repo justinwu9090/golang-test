49670a451eab134523ee9ce32caeffa32f7cb77c
Justin Wu <justinwu9090@gmail.com>
2024-10-02 16:19:24 -0700

    start shape calculations:

---
+++ structsmethodsinterfaces/perimeter.go
@@ -0,0 +1,10 @@
+package structsmethodsinterfaces
+
+func Perimeter(width, height float64) (ans float64) {
+	ans = 2 * (width + height)
+	return
+}
+
+func Area(width, height float64) float64 {
+	return width * height
+}
