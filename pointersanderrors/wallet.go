77ff1860c6cf3ede1484f60bd908af1e9b3b13b7
Justin Wu <justinwu9090@gmail.com>
2024-10-02 16:52:59 -0700

    creating type

--- pointersanderrors/wallet.go
+++ pointersanderrors/wallet.go
@@ -1,14 +1,16 @@
 package Wallet
 
+type Bitcoin int // creating type
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // this will fail since w is copied. we actually need a pointer
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance // explicit dereference is done here
 }
