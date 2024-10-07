e85b3781b8030ed839aaa446e508dd6a71e7dcd1
Justin Wu <justinwu9090@gmail.com>
2024-10-07 16:33:24 -0700

    create website checker code

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,1 +1,13 @@
 package concurrency
+
+// websitechecker is a function that takes a single URL and returns a bool
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+	return results
+}
