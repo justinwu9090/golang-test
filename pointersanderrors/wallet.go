f785f54fe101fe118d3e86f0163f46c08383e53a
Justin Wu <justinwu9090@gmail.com>
2024-10-07 12:45:18 -0700

    some comments?

--- pointersanderrors/wallet.go
+++ pointersanderrors/wallet.go
@@ -6,7 +6,10 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-// this will fail since w is copied. we actually need a pointer
+// w Wallet --> this will fail since w is copied. we actually need a pointer
+// w *Wallet will work. notice w.balance means it's dereferenced + member access
+// happened without needing -> or (*w).balance. also (*w).balance is still valid code
+// but struct pointers are automatically dereferenced
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
