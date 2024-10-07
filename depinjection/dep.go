4c4304e9cdbba347cd91fd2d9af4423e4d272e78
Justin Wu <justinwu9090@gmail.com>
2024-10-07 14:58:22 -0700

    example usage of dependency injection through servicing http requests

--- depinjection/dep.go
+++ depinjection/dep.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie\n")
+	// Greet(os.Stdout, "Elodie\n")
+	// example for when to use io writer
+
+	// when writing an HTTP handler, you're given a response writer and request pointer that was used to make the request.
+	// when you implement your server you write your response using the writer. so http.ResponseWriter also implements io.Writer.
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
