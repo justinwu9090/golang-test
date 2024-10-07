7dcce4f72bf5ef76420497ef37d689b1a4be0d85
Justin Wu <justinwu9090@gmail.com>
2024-10-07 15:03:51 -0700

    mocking test creating countdown

---
+++ mocking/mocking_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// goal:
+
+// print 3
+
+// print 3,2,1 and go!
+
+// wait a second between each line
+
+func TestCountDown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := "3"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
