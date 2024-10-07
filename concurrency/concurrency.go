e7eea585bc32afaf3bed2710162970268cf71684
Justin Wu <justinwu9090@gmail.com>
2024-10-07 16:56:45 -0700

    concurrency implemented correctly with go routine, channels, send/receiving to get rid of race condition

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,19 +1,29 @@
 package concurrency
 
-import "time"
-
 // websitechecker is a function that takes a single URL and returns a bool
 type WebsiteChecker func(string) bool
 
+// result is associated with the return value of WebsiteChecker
+type result struct {
+	string //anonymous values
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result) // create a channel of type result
 
 	for _, url := range urls {
 		// anonymous function that launches go routine, both declared and called -> () at the end of the anon function definition
 		go func(u string) {
-			results[url] = wc(url) // this will result in a race condition!
+			// send statement
+			resultChannel <- result{u, wc(u)}
 		}(url) // pass in url as value so that they don't have the same reference to url!
 	}
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		// receive statement coming out of the channel
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
 	return results
 }
