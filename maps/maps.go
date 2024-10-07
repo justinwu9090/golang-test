478b622dc86cb00b4aa94921c766ffdb1ba8f5fe
Justin Wu <justinwu9090@gmail.com>
2024-10-07 14:16:33 -0700

    check existing word in dictionary

--- maps/maps.go
+++ maps/maps.go
@@ -2,7 +2,10 @@ package main
 
 import "errors"
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 type Dictionary map[string]string
 
@@ -15,6 +18,26 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search((word))
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
+
+// gross map caveats
+// maps can be nil, which behaves like an empty map when reading. attempt to write to a nil map causes runtime panic
+// never initialize a nil map variable
+// e.g. var m map[string]string
+// do this instead:
+// var ditionary = map[string]string{} <-- the differnce is  {} and =
+// OR
+// var dictionary = make(map[string]string)
