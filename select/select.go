afc1b75f24f28864c367fb3822676a2aabc282b1
Justin Wu <justinwu9090@gmail.com>
2024-10-07 22:12:03 -0700

    typo

--- select/select.go
+++ select/select.go
@@ -20,7 +20,7 @@ func Racer(a, b string) (winner string) {
 	return b
 }
 
-func measuerResponseTime(url string) time.Duration {
+func measureResponseTime(url string) time.Duration {
 	start := time.Now()
 	http.Get(url)
 	return time.Since(start)
