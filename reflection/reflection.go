f9ba092da04607afa337c9679f967eb1c0ae9337
Justin Wu <justinwu9090@gmail.com>
2024-10-08 15:27:04 -0700

    reflection: refactor Kind() check into switch statement

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -15,11 +15,10 @@ func walk(x interface{}, fn func(string)) {
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-		// recursive walk through nested structs
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 
