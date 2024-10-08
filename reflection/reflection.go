3b8d4d9184a4ed5b53174d2fc89b4e81027bb0c9
Justin Wu <justinwu9090@gmail.com>
2024-10-08 15:06:32 -0700

    reflection first passing commit

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,8 +5,13 @@ reflection is defined as inspecting the type at runtime and seeing if it matches
 */
 package main
 
+import "reflect"
+
 // interface{} loses the type safety but now can use "any" type that you don't know at compile time.
 
 func walk(x interface{}, fn func(string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them in last in their group")
+	val := reflect.ValueOf(x) //reflect package func that returns a Value of a given variable.
+	// assumes: 1. look at first and only field
+	field := val.Field(0)
+	fn(field.String()) // 2. call String() which returns underlying value a sstring
 }
