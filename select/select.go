9df60a60138dd0638bc03ef816fb775b88c7b8a6
Justin Wu <justinwu9090@gmail.com>
2024-10-07 22:20:06 -0700

    select implementation

--- select/select.go
+++ select/select.go
@@ -6,18 +6,29 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	/*
-		http.Get(str) performs HTTP GET request against the URL
-	*/
-
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	// "select" will launch every case and only execute the case that returns first.
+	// this is a replacement for myVar := <-ch which is blocking for each val that comes out of the channel
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+
+	// always "make" channels. var ch chan struct{} is a nil channel and sending to it will do nothing
+	ch := make(chan struct{})
+	// go routine here
+	go func() {
+		// http.Get(str) performs HTTP GET request against the URL
+		http.Get(url)
+		close(ch)
+	}()
+
+	// return the channel?
+	return ch
 }
 
 func measureResponseTime(url string) time.Duration {
