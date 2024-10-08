99f9b6d381d0f0fed9e1ca052f7b22447f32052a
Justin Wu <justinwu9090@gmail.com>
2024-10-07 22:03:33 -0700

    Racer implementation

--- select/select.go
+++ select/select.go
@@ -1,5 +1,25 @@
 package main
 
-func Racer(slowURL, fastURL string) (winner string) {
+import (
+	"net/http"
+	"time"
+)
 
+func Racer(a, b string) (winner string) {
+	/*
+		http.Get(str) performs HTTP GET request against the URL
+	*/
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
