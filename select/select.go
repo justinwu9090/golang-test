20679f1a536a83fd01134126826c93fa8909e6ab
Justin Wu <justinwu9090@gmail.com>
2024-10-08 14:49:47 -0700

    added timeout of 10 seconds to the select racer

--- select/select.go
+++ select/select.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
+func Racer(a, b string) (winner string, err error) {
 	// "select" will launch every case and only execute the case that returns first.
 	// this is a replacement for myVar := <-ch which is blocking for each val that comes out of the channel
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
@@ -31,8 +34,8 @@ func ping(url string) chan struct{} {
 	return ch
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
-}
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
