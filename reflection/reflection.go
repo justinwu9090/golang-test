f3e63129e2bbb2e541dc773b66bc47dce4c704a1
Justin Wu <justinwu9090@gmail.com>
2024-10-08 15:39:16 -0700

    reflection: move around order of cases in switch statement

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -13,17 +13,19 @@ func walk(x interface{}, fn func(string)) {
 	val := getValue(x)
 
 	switch val.Kind() {
-	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
 	case reflect.String:
 		fn(val.String())
+
 	case reflect.Struct:
 		for i := 0; i < val.NumField(); i++ {
 			walk(val.Field(i).Interface(), fn)
 		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
 	}
+
 }
 
 func getValue(x interface{}) reflect.Value {
