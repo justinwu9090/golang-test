b5c54bb469bb57fc27d01fc31aa1f0e71d55a20c
Justin Wu <justinwu9090@gmail.com>
2024-10-08 16:54:44 -0700

    sync: add waitGroup Add/Wait to sync go routines (still has race condition)

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -9,11 +12,30 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		AssertValue(t, counter, 3)
+		AssertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup //syncing tool
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+
+		wg.Wait() //same as CUDA __syncthreads()
+
+		AssertCounter(t, counter, wantedCount)
 	})
 }
 
-func AssertValue(t testing.TB, got Counter, want int) {
+func AssertCounter(t testing.TB, got Counter, want int) {
 	if got.Value() != want {
 		t.Errorf("got %d want %d", got, want)
 	}
