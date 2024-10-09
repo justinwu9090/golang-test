709c5af85c8727ae98f07ae235bc581474e2072e
Justin Wu <justinwu9090@gmail.com>
2024-10-09 12:18:27 -0700

    sync: dangerous passing our counter by value which will copy the mutex

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -31,12 +31,21 @@ func TestCounter(t *testing.T) {
 
 		wg.Wait() //same as CUDA __syncthreads()
 
-		AssertCounter(t, counter, wantedCount)
+		AssertCounter(t, &counter, wantedCount)
 	})
 }
 
-func AssertCounter(t testing.TB, got Counter, want int) {
+func AssertCounter(t testing.TB, got *Counter, want int) {
 	if got.Value() != want {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
+
+/*
+Mutex allows us to add locks to our data
+WaitGroup is a means of waiting for goroutines to finish jobs
+
+decision making:
+Use channels when passing ownership of data
+Use mutexes for managing state
+*/
