cbc941be1888ebbb9212160e4db9f9016f40b2f7
Justin Wu <justinwu9090@gmail.com>
2024-10-09 16:02:34 -0700

    context: cancel vs Fetch go routine

--- context/context.go
+++ context/context.go
@@ -20,7 +20,21 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		// fetch must be done in a goroutine to race against the context's Done()
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		// use select to race said data vs the Done() which signals "cancelled".
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
