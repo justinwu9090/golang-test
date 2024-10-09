2ae46da09e8878310ba1e05251dab3792b4e9fb2
Justin Wu <justinwu9090@gmail.com>
2024-10-09 15:49:50 -0700

    context: first commit

---
+++ context/context.go
@@ -0,0 +1,25 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ======================================================
+// Interfaces
+// ======================================================
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+// ======================================================
+// Functions
+// ======================================================
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
