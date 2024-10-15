bf5977a3dffc29137cd929a12a2111e0e49fc172
Justin Wu <justinwu9090@gmail.com>
2024-10-15 11:49:08 -0700

    property: correct way of handling roman numerals

--- propertybasedtests/romannumerals.go
+++ propertybasedtests/romannumerals.go
@@ -2,12 +2,31 @@ package main
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	// builder is used to efficiently build a string using Write methods. it minimizes memory copying
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
-		result.WriteString("I")
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
+
 	return result.String()
 }
